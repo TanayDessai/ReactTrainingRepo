9cef8cd7bf10efd23e8bceaf4714e3b815c356bb
Tanay Desai <tanydesai123@gmail.com>
2023-11-13 18:24:31 +0530

    routing
    routing

--- hook-forms/src/App.js
+++ hook-forms/src/App.js
@@ -2,15 +2,31 @@ import logo from './logo.svg';
 import './App.css';
 import WithHooksForms from './components/WithHookForms';
 import WithoutHookForm from './components/WithoutHookForm';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import FirstPage from "./pages/FirstPage";
+import SecondPage from "./pages/SecondPage";
+import NestedFirst from "./pages/NestedFirst";
 
 function App() {
   return (
-    <div className="main-section">
-      <div className="section-1">
-        <WithHooksForms />
-        {/* <WithoutHookForm/> */}
-      </div>
-    </div>
+    // <div className="main-section">
+    //   <div className="section-1">
+    //     <WithHooksForms />
+    //     {/* <WithoutHookForm/> */}
+    //   </div>
+    // </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* //nested Route below */}
+        <Route path="firstPage">
+          <Route path="" index element={<FirstPage />} />
+          <Route path="nestedFirst/:id" element={<NestedFirst />} />
+        </Route>
+        <Route path="/secondPage" element={<SecondPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ hook-forms/src/pages/NestedSecond.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NestedSecond = () => {
+  return <div>NestedSecond</div>;
+};
+
+export default NestedSecond;
